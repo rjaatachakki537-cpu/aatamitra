e2a7828d8b169be714426922d1a0590197a18f6b
rjaatachakki537-cpu <rjaatachakki537@gmail.com>
2026-03-12 10:45:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,21 @@
+// sw.js
+const CACHE_NAME = 'aata-mitra-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js'
+];
+
 self.addEventListener('install', (e) => {
-  console.log('Aata Mitra SW: Installed');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  console.log('Aata Mitra SW: Installed & Cached Assets');
 });
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
