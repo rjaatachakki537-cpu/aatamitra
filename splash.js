6979fe9cb7d03dd99d986a05b22f11f8ebc48cef
rjaatachakki537-cpu <rjaatachakki537@gmail.com>
2026-03-14 17:40:03 +0100

    Update splash.js

--- splash.js
+++ splash.js
@@ -1,16 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const splash = document.getElementById('splash-screen');
-    const login = document.getElementById('login-screen');
-    const mainApp = document.getElementById('main-app');
+document.addEventListener("DOMContentLoaded", () => {
+
+    const splash = document.getElementById("splash-screen");
+    const login = document.getElementById("login-screen");
+    const mainApp = document.getElementById("main-app");
 
     setTimeout(() => {
-        splash.style.display = 'none';
-        const user = localStorage.getItem('userMobile');
-        if (user) {
-            mainApp.style.display = 'block';
-            loadProducts(); // script.js se call hoga
+
+        splash.style.display = "none";
+
+        const user = localStorage.getItem("userMobile");
+
+        if(user){
+
+            mainApp.style.display = "block";
+
+            if(typeof loadProducts === "function"){
+                loadProducts();
+            }
+
         } else {
-            login.style.display = 'block';
+
+            login.style.display = "block";
+
         }
-    }, 3000);
+
+    },3000);
+
 });
